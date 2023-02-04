af0b4f8f2e4bdedf93ef6a496f1fa4189e0beb98
Mina SaȜd <73399220+Mina-Saad-2022@users.noreply.github.com>
2023-02-04 21:28:29 +0200

    The first comment on the ecommerce project

---
+++ lang/ar/content.php
@@ -0,0 +1,14 @@
+<?php
+
+/** content translate */
+
+return [
+    'home'=>'الصفحة الرئيسية',
+    'banner'=>'بانر',
+    'new_members' => 'أعضاء جدد',
+    'sales' => 'مبيعات',
+    'likes' => 'اعجابات',
+
+
+
+];
---
+++ lang/en/content.php
@@ -0,0 +1,11 @@
+<?php
+
+/** content translate */
+
+return [
+    'home'=>'Home Page',
+    'banner'=>'Banner',
+    'new_members' =>'New Members',
+    'sales' => 'Sales',
+    'likes' => 'Likes',
+];
